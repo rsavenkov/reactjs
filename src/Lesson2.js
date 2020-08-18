0ace19b261afbf3caaa2fa26c709a32e2abe7c38
rsavenkov <lubajnka@mail.ru>
2020-08-18 18:01:45 +0300

    Lesson 2 - colored clock

--- src/Lesson2.js
+++ src/Lesson2.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+const colors = ['red', 'green', 'blue', 'orange', 'grey', 'green', 'yellow', 'purple', 'white', 'marine'];
+
 class Clock extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {date: new Date()};
+        this.state = {
+            date: new Date(),
+            color: colors[0]
+        };
     }
 
     componentDidMount() {
@@ -19,13 +24,14 @@ class Clock extends React.Component {
 
     tick() {
         this.setState({
-            date: new Date()
+            date: new Date(),
+            color: colors[Math.floor(Math.random() * 10)]
         });
     }
 
     render() {
         return (
-            <div>
+            <div style={{backgroundColor: this.state.color}}>
                 <h1>Hello from clock example</h1>
                 <h2>Time is {this.state.date.toLocaleTimeString()}</h2>
             </div>
